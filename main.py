13e176e4caffdbe8a27f50ea3e70c3c5f56ba9f2
Alpha <alpha6662017@outlook.com>
2022-11-01 16:00:33 +0800

    main-opcode計算功能完成

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ class num:
 
     def __init__(self):
         self.decNum:int=0
-        self.hexNum:str='0000'
+        self.hexNum:str='000000'
     
-    def setHex(self,setHex:str,passSaveDec=False):
-        if len(setHex)>4:
+    def setHex(self,setHex:str,passSaveDec=False,maxlen=4):
+        if len(setHex)>maxlen:
             print('[error] Hex overflow!')
             return 
         else:
             self.hexNum=setHex
             if not passSaveDec:
                 self.decNum=int(setHex,16)
-            while len(self.hexNum)<4:
+            while len(self.hexNum)<maxlen:
                 self.hexNum='0'+self.hexNum
     
     def next(self,step=3):
@@ -32,13 +32,49 @@ def resChecker(chunk:list[str])->int:
     else:
         return 3
 
-opcode={}
+def opcodeGen(chunk:list[str],symbolTable):
+    opTable={"ADD":"18","AND":"40","COMP":"28","DIV":"24","J":"3C","JEQ":"30","JGT":"34","JLT":"38","JSUB":"48","LDA":"00","LDCH":"50","LDL":"08","LDX":"04","MUL":"20","OR":"44","RD":"D8","RSUB":"4C","STA":"0C","STCH":"54","STL":"14","STSW":"E8","STX":"10","SUB":"1C","TD":"E0","TIX":"2C","WD":"DC"}
+    nullObjectList={'START','END','RESW','RESB'}
+    if chunk[0]!='.':
+        if chunk[1] in nullObjectList:
+            return ''
+        else:
+            if chunk[1]!='BYTE' and chunk[1]!='WORD':
+                if len(chunk)==2:
+                    return opTable[chunk[1]]+'0000'
+                elif chunk[2] and not chunk[2].endswith(',X'):
+                    return opTable[chunk[1]]+symbolTable[chunk[2]] 
+                else:
+                    temp=num()
+                    temp.setHex(opTable[chunk]+symbolTable[chunk[2].split(',')[1]])
+                    temp.next(step=32768)
+                    return temp.hexNum
+            else:
+                if chunk[1]=='WORD':
+                    temp=num()
+                    temp.setHex(hex(int(chunk[2],base=16))[2:],maxlen=6)
+                    return temp.hexNum
+                elif chunk[1]=='BYTE' and chunk[2].startswith('C'):
+                    asc=[hex(ord(char))[2:] for char in chunk[2].split("'")[1]]
+                    temp=""
+                    for code in asc:
+                        temp=temp+code
+                    return temp
+                elif chunk[1]=='BYTE' and chunk[2].startswith('X'):
+                    return chunk[2].split("'")[1]
+    else:
+        return ''
+                
+                
+        
 
 def main(path):
     nowLocation=num()
+    startLocation=num()
     locationList=list()
     programName=""
     symbolTable=dict()
+    objectFunC=list()
     asmFile=open(path,'r',encoding='utf-8')
     asmData=asmFile.readlines()
     asm=[data.strip().split() for data in asmData]
@@ -46,6 +82,7 @@ def main(path):
         if chunk[0]!='.':    
             if chunk[1]=='START':
                 programName=chunk[0]
+                startLocation.setHex(chunk[2],maxlen=6)
                 nowLocation.setHex(chunk[2])
                 locationList.append(nowLocation.hexNum)
             else:
@@ -72,6 +109,11 @@ def main(path):
             symbolTable[chunk[0]]=locationList[iter]
         iter+=1
     symTableOutput.close()
+
+    for chunk in asm:
+        objectFunC.append(opcodeGen(chunk=chunk,symbolTable=symbolTable))
+
+    #print(objectFunC)
                 
             
 
