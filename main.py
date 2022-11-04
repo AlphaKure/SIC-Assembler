f12f340701f3f006b56c0d05a4fc2e9b95b068b4
Alpha <alpha6662017@outlook.com>
2022-11-04 18:58:27 +0800

    main-功能完成九成

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-class num:
+def transHex(inputHex:str=None,dec:int=None,bits=4)->str:
+    if inputHex!=None:
+        while len(inputHex)<bits:
+            inputHex='0'+inputHex
+        return inputHex.upper()
+    if dec!=None:
+        temp=hex(dec)[2:]
+        while len(temp)<bits:
+            temp='0'+temp
+        return temp.upper()
 
-    def __init__(self):
-        self.decNum:int=0
-        self.hexNum:str='000000'
-    
-    def setHex(self,setHex:str,passSaveDec=False,maxlen=4):
-        if len(setHex)>maxlen:
-            print('[error] Hex overflow!')
-            return 
-        else:
-            self.hexNum=setHex
-            if not passSaveDec:
-                self.decNum=int(setHex,16)
-            while len(self.hexNum)<maxlen:
-                self.hexNum='0'+self.hexNum
-    
-    def next(self,step=3):
-        self.decNum+=step
-        self.setHex(hex(self.decNum)[2:],passSaveDec=True)
+def hexAdder(hex:str,addDec:int,bits=4)->str:
+    return transHex(dec=int(hex,base=16)+addDec,bits=bits)
 
-def resChecker(chunk:list[str])->int:
+def commandLocCal(chunk:list[str])->int:
     if chunk[1]=='RESW':
         return int(chunk[2])*3
     elif chunk[1]=='RESB':
@@ -45,32 +38,51 @@ def opcodeGen(chunk:list[str],symbolTable):
                 elif chunk[2] and not chunk[2].endswith(',X'):
                     return opTable[chunk[1]]+symbolTable[chunk[2]] 
                 else:
-                    temp=num()
-                    temp.setHex(opTable[chunk]+symbolTable[chunk[2].split(',')[1]])
-                    temp.next(step=32768)
-                    return temp.hexNum
+                    temp=transHex(inputHex=opTable[chunk[1]]+symbolTable[chunk[2].split(',')[0]],bits=6)
+                    temp=hexAdder(hex=temp,addDec=32768,bits=6)
+                    return temp
             else:
                 if chunk[1]=='WORD':
-                    temp=num()
-                    temp.setHex(hex(int(chunk[2],base=16))[2:],maxlen=6)
-                    return temp.hexNum
+                    temp=transHex(inputHex=hex(int(chunk[2]))[2:],bits=6)
+                    return temp
                 elif chunk[1]=='BYTE' and chunk[2].startswith('C'):
                     asc=[hex(ord(char))[2:] for char in chunk[2].split("'")[1]]
                     temp=""
                     for code in asc:
                         temp=temp+code
-                    return temp
+                    return temp.upper()
                 elif chunk[1]=='BYTE' and chunk[2].startswith('X'):
-                    return chunk[2].split("'")[1]
+                    return chunk[2].split("'")[1].upper()
     else:
-        return ''
-                
-                
-        
+        return None
+                                
+def createObjectProgram(objectProgramFile,programName,startLocation,objectFunc:list,locationList):
+    while len(programName)<6:
+        programName=programName+' '
+    objectProgramFile.write(f'H{programName}{startLocation}{transHex(dec=int(locationList[-1],16)-int(startLocation,16),bits=6)}\n')
+    temp=''
+    iter=0
+    jumpFlag=False
+    firstlocation=transHex(inputHex=locationList[iter],bits=6)
+    for location in locationList:
+        if iter!=len(locationList)-1:
+            if (int(locationList[iter+1],16)-int(location,16))>4:
+                jumpFlag=True
+        if objectFunc[iter]!='' and objectFunc[iter]!=None:
+            if iter!=len(locationList)-1:
+                temp=temp+objectFunc[iter]
+        if len(temp)>55 or (iter==len(locationList)-1 and temp!='')or jumpFlag:
+            objectProgramFile.write(f'T{firstlocation}{transHex(dec=len(temp)//2,bits=2)}{temp}\n')
+            temp=''
+            jumpFlag=False
+            if iter!=len(locationList)-1:
+                firstlocation=transHex(inputHex=locationList[iter+1],bits=6)
+        iter+=1
+
+    objectProgramFile.write(f'E{startLocation}\n')
+    objectProgramFile.close()
 
 def main(path):
-    nowLocation=num()
-    startLocation=num()
     locationList=list()
     programName=""
     symbolTable=dict()
@@ -82,36 +94,50 @@ def main(path):
         if chunk[0]!='.':    
             if chunk[1]=='START':
                 programName=chunk[0]
-                startLocation.setHex(chunk[2],maxlen=6)
-                nowLocation.setHex(chunk[2])
-                locationList.append(nowLocation.hexNum)
+                startLocation=transHex(inputHex=chunk[2],bits=6)
+                nowLocation=transHex(inputHex=chunk[2],bits=4)
+                locationList.append(nowLocation)
             else:
-                locationList.append(nowLocation.hexNum)
-                nowLocation.next(step=resChecker(chunk))
+                locationList.append(nowLocation)
+                nowLocation=hexAdder(hex=nowLocation,addDec=commandLocCal(chunk))
         else:
-            locationList.append(nowLocation.hexNum)
+            locationList.append(nowLocation)
     
             
-    locationOutput=open('./loc.txt','w',encoding='utf-8')
-    symTableOutput=open('./SYMTable.txt','w',encoding='utf-8')
+    locationFile=open('./loc.txt','w',encoding='utf-8')
+    outputFile=open('./output.txt','w',encoding='utf-8')
+    objectcodeFile=open('./objectcode.txt','w',encoding='utf-8')
 
+    #輸出loc.txt
     iter=0
     for line in asmData:
-        locationOutput.writelines(f'{locationList[iter]} {line}')
+        locationFile.writelines(f'{locationList[iter]} {line} ')
         iter+=1
-    locationOutput.close()
-    asmFile.close()
+    locationFile.close()
 
-    iter=0
+    #寫SymbolTable
+    iter=0 
     for chunk in asm:
         if chunk[0]!='.' and chunk[0]!='-' and iter!=0:
-            symTableOutput.writelines(f'{chunk[0]} 0x{locationList[iter]}\n')
             symbolTable[chunk[0]]=locationList[iter]
         iter+=1
-    symTableOutput.close()
-
+    
+    #生成指令碼
     for chunk in asm:
         objectFunC.append(opcodeGen(chunk=chunk,symbolTable=symbolTable))
+
+    #輸出output.txt
+    iter=0
+    for line in asmData:
+        if objectFunC[iter]==None:
+            outputFile.writelines(f'{locationList[iter]} {line.rstrip()} \n')
+        else:
+            outputFile.writelines(f'{locationList[iter]} {line.rstrip()}    {objectFunC[iter]} \n')
+        iter+=1
+    outputFile.close()
+    
+    #輸出及生成objectcode.txt
+    createObjectProgram(objectcodeFile,programName,startLocation,objectFunC,locationList)
 
     #print(objectFunC)
                 
@@ -119,4 +145,4 @@ def main(path):
 
     
 if __name__=='__main__':
-    main('./test.txt')
+    main('./a.txt')
